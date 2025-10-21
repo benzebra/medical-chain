2dabf997a79f68882f3b22663d8447ee4667975c
benzebra <filippobrajucha@gmail.com>
2025-10-21 09:58:38 +0200

    last fix

--- src/js/transactions.js
+++ src/js/transactions.js
@@ -11,7 +11,7 @@ App = {
 
     init: async function() {
         ObjectAPI.getAllObjects().then(objects => {
-            objects = objects.slice(-10);
+            // objects = objects.slice(-20);
             objects.forEach(function(object) {
                 if (object.blockchain_status === 'clean') {
                     var $tbody = $('#transactionTableCleaning tbody');
@@ -85,86 +85,40 @@ App = {
         });
     }, 
 
-    bindEvents: function() {
-        // $(document).on('click', '.btn-cleaned', App.handleCleaning);
-        // $(document).on('click', '.btn-used', App.handleUsing);
-    },
-
-    addToList: function() {
-        event.preventDefault && event.preventDefault();
-
-        var usingInstance;
-
-        web3.eth.getAccounts(function(error, accounts) {
-            if (error) {
-                console.log(error);
-            }
-
-            var account = accounts[0];
-
-            App.contracts.Cleaning.deployed().then(function(instance) {
-                usingInstance = instance;
-
-                // console.log(account);
-
-                var valCleaned = usingInstance.getObjectsCleaned({from: account});
-                var valUsed = usingInstance.getObjectsUsed({from: account});
-
-                // App.parseTableCleaning(valCleaned);
-                // App.parseTableUsing(valUsed);
-
-                return true
-            }).catch(function(err) {
-                console.log(err.message);
-            });
-        });
-    },
-
-    // parseTableCleaning: function(val) {
-    //     val.then(function(objects) {
-    //         console.log(objects)
-    //         var $tbody = $('#transactionTableCleaning tbody');
-    //         if ($tbody.length === 0) {
-    //             $tbody = $('<tbody>');
-    //             $('#transactionTableCleaned').append($tbody);
-    //         }
-    //         $tbody.empty();
-    //         for (var i = 0; i < objects.length; i++) {
-    //             if(objects[i] != '0x0000000000000000000000000000000000000000'){
-    //                 var row = '<tr>';
-    //                 row += '<td>' + i + '</td>';
-    //                 row += '<td>' + objects[i] + '</td>';
-    //                 row += '</tr>';
-    //                 $tbody.append(row);
-    //             }
-    //         }
-    //     }).catch(function(err) {
-    //         console.log(err.message);
-    //     });
+    // bindEvents: function() {
+    //     // $(document).on('click', '.btn-cleaned', App.handleCleaning);
+    //     // $(document).on('click', '.btn-used', App.handleUsing);
     // },
 
-    // parseTableUsing: function(val) {
-    //     val.then(function(objects) {
-    //         var $tbody = $('#transactionTableUsing tbody');
-    //         if ($tbody.length === 0) {
-    //             $tbody = $('<tbody>');
-    //             $('#transactionTableUsed').append($tbody);
-    //         }
-    //         $tbody.empty();
-    //         for (var i = 0; i < objects.length; i++) {
-    //             if(objects[i] != '0x0000000000000000000000000000000000000000'){
-    //                 var row = '<tr>';
-    //                 row += '<td>' + i + '</td>';
-    //                 row += '<td>' + objects[i] + '</td>';
-    //                 row += '</tr>';
-    //                 $tbody.append(row);
-    //             }
-    //         }
-    //     }).catch(function(err) {
-    //         console.log(err.message);
-    //     });
-    // }
+    // addToList: function() {
+    //     event.preventDefault && event.preventDefault();
 
+    //     var usingInstance;
+
+    //     web3.eth.getAccounts(function(error, accounts) {
+    //         if (error) {
+    //             console.log(error);
+    //         }
+
+    //         var account = accounts[0];
+
+    //         App.contracts.Cleaning.deployed().then(function(instance) {
+    //             usingInstance = instance;
+
+    //             // console.log(account);
+
+    //             var valCleaned = usingInstance.getObjectsCleaned({from: account});
+    //             var valUsed = usingInstance.getObjectsUsed({from: account});
+
+    //             // App.parseTableCleaning(valCleaned);
+    //             // App.parseTableUsing(valUsed);
+
+    //             return true
+    //         }).catch(function(err) {
+    //             console.log(err.message);
+    //         });
+    //     });
+    // },
 };
 
 $(function() {
